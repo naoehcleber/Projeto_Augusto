7505e1e5ed0902f604c820a8f06b3a875f78d48d
Júlia Vilela <125268764+juliaavilelaa@users.noreply.github.com>
2023-12-01 15:52:31 -0300

    commit

--- structs.h
+++ structs.h
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct caso
 {
-    /* data */
-    //nome
     char nome[50];
     int idade;
-    //avaliacao de 0 a 10
     int avaliacao;
-    //M ou F
     char genero;
-    
     int contador;
-
 } c[50];
